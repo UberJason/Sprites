47dd234a757839109e94b78acbab60e6bb264e5e
Jason Ji <uberjason@gmail.com>
2016-06-16 08:54:41 -0700

    Initial Commit

---
+++ Sprites WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Sprites WatchKit Extension
+//
+//  Created by Jason Ji on 6/16/16.
+//  Copyright © 2016 Jason Ji. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: AnyObject?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
